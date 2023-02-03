ceca25b854d01767306ac24860cf2df235c00d5e
nawaz quazi <nawazquazi356@gmail,com>
2023-02-03 21:50:18 +0530

    Reverse a number

---
+++ Questions_6.java
@@ -0,0 +1,10 @@
+public class Questions_6 {
+    public static void main(String[] args) {
+        int n=754;
+        while (n>0){
+            int r=n%10;
+            System.out.println(r);
+            n/=10;
+        }
+    }
+}
