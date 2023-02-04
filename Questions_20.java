6d941ea0c6ee565a0e2d2572953f782c36810d2b
nawaz quazi <nawazquazi356@gmail,com>
2023-02-04 21:06:43 +0530

    pattern 7

---
+++ Questions_20.java
@@ -0,0 +1,18 @@
+public class Questions_20 {
+
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    public static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j || i + j == n + 1) {
+                    System.out.print("*\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
