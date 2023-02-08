9d6bff3a168bf4bb2a520f3dc8087b4bc09259ea
nawaz quazi <nawazquazi356@gmail,com>
2023-02-08 11:47:19 +0530

    Any base to addtion

---
+++ Questions_40.java
@@ -0,0 +1,22 @@
+public class Questions_40 {
+    public static void main(String[] args) {
+
+    }
+        public static int any_base_to_Addition(int base, int number1, int number2) {
+            int rv = 0;
+            int p = 1;
+            int c = 0;
+            while (number1 > 0 || number2 > 0 || c > 0) {
+                int d1 = number1 % 10;
+                int d2 = number2 % 10;
+                number1 = number1 / 10;
+                number2 = number2 / 10;
+                int d = d1 + d2 + c;
+                c = d / base;
+                d = d % base;
+                rv += d * p;
+                p = p * 10;
+            }
+            return rv;
+        }
+    }
