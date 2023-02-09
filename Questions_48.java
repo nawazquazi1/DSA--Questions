39ee099941a369401f12a3e3b7a8ab1ac6a54d65
nawaz quazi <nawazquazi356@gmail,com>
2023-02-09 15:59:31 +0530

    Duplicats brackets

--- Questions_48.java
+++ Questions_48.java
@@ -1,2 +1,34 @@
+import java.util.Stack;
+
 public class Questions_48 {
+    public Questions_48() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(duplicate("((a+b)+(c+d))"));
+        System.out.println(duplicate("(a+b)+((c+d))"));
+    }
+
+    public static boolean duplicate(String exp) {
+        Stack<Character> stack = new Stack();
+
+        for(int i = 0; i < exp.length(); ++i) {
+            char c = exp.charAt(i);
+            if (c != ')') {
+                stack.push(c);
+            } else {
+                if ((Character)stack.peek() == '(') {
+                    return true;
+                }
+
+                while((Character)stack.peek() != '(') {
+                    stack.pop();
+                }
+
+                stack.pop();
+            }
+        }
+
+        return false;
+    }
 }
