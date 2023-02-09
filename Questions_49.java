5a2fedb6276056296a7dc73e5fef6c2c804ca92a
nawaz quazi <nawazquazi356@gmail,com>
2023-02-09 16:13:48 +0530

    Balnce Bracket

--- Questions_49.java
+++ Questions_49.java
@@ -1,2 +1,63 @@
+import java.util.Stack;
+
 public class Questions_49 {
+    public Questions_49() {
+    }
+
+    public static void main(String[] args) {
+        balncedbracket("[(a+b)+{(c+d)*(e/f)}]");
+        balncedbracket("[(a+b)+{(c+d)*(e/f)]}");
+        balncedbracket("[(a+b)+{(c+d)*(e/f)}");
+        balncedbracket("[(a+b)+{(c+d)*(e/f)}])");
+    }
+
+    public static void balncedbracket(String exp) {
+        Stack<Character> st = new Stack();
+
+        for(int i = 0; i < exp.length(); ++i) {
+            char ch = exp.charAt(i);
+            if (ch != '(' && ch != '[' && ch != '{') {
+                boolean b;
+                if (ch == ')') {
+                    b = handel(st, '(');
+                    if (!b) {
+                        System.out.println(false);
+                        return;
+                    }
+                } else if (ch == ']') {
+                    b = handel(st, '[');
+                    if (!b) {
+                        System.out.println(false);
+                        return;
+                    }
+                } else if (ch == '}') {
+                    b = handel(st, '{');
+                    if (!b) {
+                        System.out.println(false);
+                        return;
+                    }
+                }
+            } else {
+                st.push(ch);
+            }
+        }
+
+        if (st.size() == 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+    }
+
+    public static boolean handel(Stack<Character> st, char corres) {
+        if (st.size() == 0) {
+            return false;
+        } else if ((Character)st.peek() != corres) {
+            return false;
+        } else {
+            st.pop();
+            return true;
+        }
+    }
 }
